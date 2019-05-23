f95264cad9808731dec41524a344a952cbf22827
Howard Liu <howardliu94@gmail.com>
2019-05-22 17:03:39 -0700

    Palindrome number

--- palindrome_num.py
+++ palindrome_num.py
@@ -17,3 +17,37 @@
 # Follow up:
 
 # Coud you solve it without converting the integer to a string?
+import math
+def palindrome_num(num):
+    if num < 0:
+        return False
+    elif num < 10:
+        return True
+    n = int(math.log(num, 10)) + 1 # number of digits
+    stack = []
+    for idx in range(n//2):
+        digit = num // 10**idx % 10
+        stack.append(digit)
+    startIdx = n//2
+    if n % 2 != 0:
+        startIdx = n//2 + 1
+    for idx in range(startIdx, n):
+        digit = num // 10**idx % 10
+        if stack.pop() != digit:
+            return False
+    return True
+    
+
+
+
+
+
+print(palindrome_num(120) == False)
+print(palindrome_num(1) == True)
+print(palindrome_num(9) == True)
+print(palindrome_num(12) == False)
+print(palindrome_num(245929542) == True)
+print(palindrome_num(24592954) == False)
+print(palindrome_num(2459229542) == True)
+
+
