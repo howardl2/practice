9bf540150348c08175942f1d078ca762d154074f
Howard Liu <howardliu94@gmail.com>
2019-07-01 17:12:26 -0700

    Add new problem

---
+++ space_words_from_dictionary.py
@@ -0,0 +1,12 @@
+# Given a string and a dictionary, add spaces in the string between words that are in the dictionary
+
+# Example 1:
+# s = "wowthereisasentence"
+# dictionary = {
+#   "wow": "wow",
+#   "there": "there",
+#   "is": "is",
+#   "a": "a",
+#   "sentence": "sentence"
+# }
+# Output: "wow there is a sentence"
