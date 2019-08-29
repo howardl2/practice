8c15aea65f869354ba46dea1f9be73896706e309
Howard Liu <howardliu94@gmail.com>
2019-08-28 17:44:09 -0700

    Integer to roman

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -39,3 +39,32 @@
 # Input: 1994
 # Output: "MCMXCIV"
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+def intToRoman(num: int) -> str:
+    vocabulary = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I"
+    }
+
+    roman = ""
+    for value in vocabulary:
+        if num >= value:
+            times = num // value
+            num -= value * times
+            for time in range(times):
+                roman += vocabulary[value]
+    return roman
+
+print(intToRoman(1994) == "MCMXCIV")
+print(intToRoman(8) == "VIII")
